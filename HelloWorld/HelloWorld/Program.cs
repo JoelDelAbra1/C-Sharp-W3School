e86d626d8f1bda08b7d07f2ef749f24c422a61f9
JoelDelAbra1 <78566418+JoelDelAbra1@users.noreply.github.com>
2023-08-12 19:18:51 -0600

    Se agrgegan los primeros

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(3+3); // Este es un comentario
+/* Y este es un comentario de varias
+   Lineas */
+Console.Write("No inserta una nueva linea");
+Console.Write("--Como lo es esto jajaja");
---
+++ TiposDeDatos/TiposDeDatos/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+/* Tipos de variable:
+ * Int
+ * Double
+ * char  (Solo uno) ""
+ * string ""
+ * bool
+ 
+
+ * Declarar variables
+ * type  nameVariable = value; */
+
+string name = "Joel";
+Console.WriteLine("Hello" + name);
+
+int myNum = 25;
+Console.WriteLine(myNum);
+
+const double Pi = 3.14;
+Console.WriteLine( "Soy una constante: " + Pi);
+
+int x = 10,y = 20,z = 30 ;
+Console.WriteLine(x+y+z);
